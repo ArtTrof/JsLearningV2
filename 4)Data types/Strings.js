4f70ff51a96bbba34ad3483088f22bb4d534d786
ArtTrof <randallsng@gmail.com>
2023-10-01 15:00:44 +0200

    strings

---
+++ 4)Data types/Strings.js
@@ -0,0 +1,15 @@
+let guestList = "Guests:\n * John\n * Pete\n * Mary";
+console.log(guestList);
+
+let str1 = "Hello\nWorld"; // two lines using a "newline symbol"
+// two lines using a normal newline and backticks
+let str2 = `Hello
+World`;
+console.log(str1.at(0));
+console.log(str1.toUpperCase());
+console.log(str1.indexOf("Hello"));
+console.log(str1.includes("Hello"));
+console.log(str1.slice(0,4));//Hell
+console.log(str1.substring(0,4));//Hell
+
+console.log('a'>'Z');//true
