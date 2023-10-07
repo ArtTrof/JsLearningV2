be105d4b69d84594804a298ab4d9b7d0f763aebb
ArtTrof <randallsng@gmail.com>
2023-10-07 14:29:46 +0200

    recursion and stack

---
+++ 4)Data types/RecursionAndStack.js
@@ -0,0 +1,70 @@
+function pow(x, n) {
+    if (n == 1) {
+        return x;
+    } else {
+        return x * pow(x, n - 1);
+    }
+};
+
+/* let company = {
+    sales: [{
+        name: 'John',
+        salary: 1000
+    }, {
+        name: 'Alice',
+        salary: 1600
+    }],
+
+    development: {
+        sites: [{
+            name: 'Peter',
+            salary: 2000
+        }, {
+            name: 'Alex',
+            salary: 1800
+        }],
+
+        internals: [{
+            name: 'Jack',
+            salary: 1300
+        }]
+    }
+}; */
+let company = { // the same object, compressed for brevity
+    sales: [{ name: 'John', salary: 1000 }, { name: 'Alice', salary: 1600 }],
+    development: {
+        sites: [{ name: 'Peter', salary: 2000 }, { name: 'Alex', salary: 1800 }],
+        internals: [{ name: 'Jack', salary: 1300 }]
+    }
+};
+function sumSalaries(department) {
+    if (Array.isArray(department)) { // case (1)
+        return department.reduce((prev, current) => prev + current.salary, 0); // sum the array
+    } else { // case (2)
+        let sum = 0;
+        for (let subdep of Object.values(department)) {
+            sum += sumSalaries(subdep); // recursively call for subdepartments, sum the results
+        }
+        return sum;
+    }
+};
+
+/* let list = {
+    value: 1,
+    next: {
+        value: 2,
+        next: {
+            value: 3,
+            next: {
+                value: 4,
+                next: null
+            }
+        }
+    }
+}; */
+//alternative
+let list = { value: 1 };
+list.next = { value: 2 };
+list.next.next = { value: 3 };
+list.next.next.next = { value: 4 };
+list.next.next.next.next = null;
